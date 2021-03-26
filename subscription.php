eaf17a3b4050d2583251ac88e123e69f31a58fe1
rocky1207 <mjrocky1918@gmail.com>
2021-03-27 00:19:17 +0100

    First subscrition.php commit

---
+++ subscription.php
@@ -0,0 +1,60 @@
+<?php
+include "partials/header.php";
+?>
+    <section class="subscription">
+        <article class="container">
+            <div>
+                <h2>Subscription from $15/month</h2>
+                <h4>Start a free trial now</h4>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
+                Consectetur rem modi officia dolorum impedit ad, 
+                porro aliquid doloribus quaerat voluptates qui, nihil tempora hic, saepe 
+                blanditiis nobis assumenda earum? Facilis temporibus iure maiores rerum, debitis deserunt ut dolorum. Culpa eligendi
+                molestias amet rem quo quibusdam eum fuga quam dolorum error?</p>
+                <button>Start free trial</button>
+            </div>
+            <div>
+                <ul>
+                    <li>Play any track</li>
+                    <li>Listen offine</li>
+                    <li>No add interruptions</li>
+                    <li>Unlimited skips</li>
+                    <li>High quality</li>
+                    <li>Shuffle play</li>
+                </ul>
+            </div>
+        </article>
+    </section>
+    <!-- artists -->
+    <section class="container concept">
+        <article>
+            <h2>Our Concept & artists</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+            Deleniti corporis velit excepturi ipsam quidem nisi officiis veniam,
+            molestiae, commodi rem. Repudiandae id officiis repellat corporis eum accusamus 
+            iusto sapiente laboriosam. Lorem ipsum dolor sit amet, consectetur adipisicing elit.  
+            Deleniti corporisvelit excepturi ipsam quidem nisi officiis veniam, molestiae, commodi rem. 
+            Repudiandae id officiis repellat corporis eum accusamus iusto sapiente laboriosam.</p>
+        </article>
+        <article>
+            <div>
+                <img src="img/subscription1.jpg" alt="">
+                <h3>No add interruptions</h3>
+            </div>
+            <div>
+                <img src="img/subscription2.jpg" alt="">
+                <h3>High Quality</h3>
+            </div>
+            <div>
+                <img src="img/subscription3.jpg" alt="">
+                <h3>Listen Offline</h3>
+            </div>
+            <div>
+                <img src="img/subscription4.jpg" alt="">
+                <h3>Download Music</h3>
+            </div>
+        </article>
+    </section>
+<?php
+include "partials/footer.php";
+?>
