5975ac9bea0a987d07377bf8919205d66587461c
rocky1207 <mjrocky1918@gmail.com>
2021-03-27 00:06:51 +0100

    First index.php commit

---
+++ index.php
@@ -0,0 +1,36 @@
+<?php
+    include "partials/header.php";
+?>
+  <!--  <h1 style="font-size: 9rem; text-align: center">Home</h1> -->
+  <!-- header -->
+  <header class="mainHeader">
+    <div>
+        <h1><span>Music </span>for everyone</h1>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
+        Laborum culpa, inventore repudiandae ratione hic autem magni vitae provident quos sunt, est sit error odit. 
+        Aliquid cum tempore molestiae. Cumque, iste?</p>
+        <button>Start free trial</button>
+    </div>
+    <div>
+        <img src="img/header.png">
+    </div>
+    <div></div>
+ </header>
+ <!-- unlimited -->
+ <section class="unlimited container">
+    <article>
+        <h2>Unlimited Acceess to 100K tarcks</h2>
+        
+    </article>
+    <article>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+    Modi ab, iusto non facilis possimus ipsum! Quisquam quaerat ab consequatur 
+    repellendus quas ducimus aliquid optio voluptates natus? Nulla earum nostrum 
+    doloremque asperiores vel repellendus iure, doloribus debitis impedit dolorem 
+    ratione autem ut veritatis, nesciunt quas, enim natus porro quam ab at!</p>
+    <button>Try it now</button>
+    </article>
+ </section>
+<?php
+    include "partials/footer.php";
+?>
