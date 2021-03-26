ef5e48d36e0e36f0ac51522dcc8a6683ebe4d381
rocky1207 <mjrocky1918@gmail.com>
2021-03-27 00:11:01 +0100

    First how-it-works.php commit

---
+++ how-it-works.php
@@ -0,0 +1,64 @@
+<?php
+include "partials/header.php";
+?>
+    <!--hovHeader -->
+    <section class="hivHeader">
+        <h2 class="container">How it works</h2>
+    <article class="container">
+        <div>
+            <i class="fas fa-user-circle"></i>
+            <h3>Create account</h3>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
+            Quidem modi at aliquam eos doloribus provident dolorum maxime 
+            illum distinctio beatae.</p>
+         </div>
+         <div>
+            <i class="fas fa-drum"></i>
+            <h3>Create account</h3>
+            <p>orem ipsum dolor, sit amet consectetur adipisicing elit. 
+            Quidem modi at aliquam eos doloribus provident dolorum maxime 
+            illum distinctio beatae.</p>
+         </div>
+         <div>
+            <i class="fas fa-cloud-download-alt"></i>
+            <h3>Create account</h3>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
+            Quidem modi at aliquam eos doloribus provident dolorum maxime 
+            illum distinctio beatae.</p>
+         </div>
+    </article>
+    
+</section>
+<!-- artists -->
+<section class="container artists">
+        <article>
+            <h2>Our Concept & artists</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+            Deleniti corporis velit excepturi ipsam quidem nisi officiis veniam,
+            molestiae, commodi rem. Repudiandae id officiis repellat corporis eum accusamus 
+            iusto sapiente laboriosam. Lorem ipsum dolor sit amet, consectetur adipisicing elit.  
+            Deleniti corporisvelit excepturi ipsam quidem nisi officiis veniam, molestiae, commodi rem. 
+            Repudiandae id officiis repellat corporis eum accusamus iusto sapiente laboriosam.</p>
+        </article>
+        <article>
+            <div>
+                <img src="img/artists1.jpg" alt="">
+                <h3>Soul music</h3>
+            </div>
+            <div>
+                <img src="img/artists2.jpg" alt="">
+                <h3>Live concerts</h3>
+            </div>
+            <div>
+                <img src="img/artists3.jpg" alt="">
+                <h3>DJ set</h3>
+            </div>
+            <div>
+                <img src="img/artists4.jpg" alt="">
+                <h3>Live streams</h3>
+            </div>
+        </article>
+    </section>
+    <?php
+include "partials/footer.php";
+?>
